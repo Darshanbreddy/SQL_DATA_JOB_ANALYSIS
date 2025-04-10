619f371f082d636aa1879b8738282292bcad5fb9
Darshanbreddy <brdarshan.2000@gmail.com>
2025-04-10 13:35:08 +0530

    final_changes

---
+++ project/most_optimal_skills.sql
@@ -0,0 +1,36 @@
+/*Analyzing In-Demand Skills & Salaries for Remote Data Analyst Roles
+ This SQL query identifies the top 25 most in-demand skills for remote Data Analyst roles with available salary information. It also calculates the average salary associated with each skill.
+ */
+WITH skills_demand AS (
+    SELECT skills_dim.skill_id,
+        skills_dim.skills,
+        COUNT(skills_job_dim.job_id) AS demand_count
+    FROM job_postings_fact
+        INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+        INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+    WHERE job_title_short = 'Data Analyst'
+        AND salary_year_avg IS NOT NULL
+        AND job_work_from_home = True
+    GROUP BY skills_dim.skill_id
+),
+average_salary AS (
+    SELECT skills_job_dim.skill_id,
+        ROUND(AVG(job_postings_fact.salary_year_avg), 0) AS avg_salary
+    FROM job_postings_fact
+        INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+        INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+    WHERE job_title_short = 'Data Analyst'
+        AND salary_year_avg IS NOT NULL
+        AND job_work_from_home = True
+    GROUP BY skills_job_dim.skill_id
+)
+SELECT skills_demand.skill_id,
+    skills_demand.skills,
+    demand_count,
+    avg_salary
+FROM skills_demand
+    INNER JOIN average_salary ON skills_demand.skill_id = average_salary.skill_id
+WHERE demand_count > 10
+ORDER BY avg_salary DESC,
+    demand_count DESC
+LIMIT 25
--- project/top_paying_skills.sql
+++ project/top_paying_skills.sql
@@ -1,31 +1,116 @@
-WITH top_pyaing_jobs AS(
-    SELECT job_id,
-        job_title,
-        job_location,
-        salary_year_avg,
-        name AS company_name
-    FROM job_postings_fact
-        LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
-    WHERE job_title_short = 'Data Analyst'
-        AND job_country = 'India'
-        AND salary_year_avg IS NOT NULL
-    ORDER BY salary_year_avg DESC
-    LIMIT 10
-)
-SELECT top_pyaing_jobs.*,
-    skills
-from top_pyaing_jobs
-    INNER JOIN skills_job_dim ON top_pyaing_jobs.job_id = skills_job_dim.job_id
-    INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
-ORDER BY salary_year_avg DESC;
-/*
- Insights:
- 
- SQL and Power BI are the most in-demand skills, each appearing in 5 listings.
- 
- Python, Cloud Platforms (AWS, Azure), and Excel are also frequently sought.
- 
- Big Data tools like Spark, Hadoop, and Databricks also appear, indicating a preference for data engineering capabilities.
- 
- Emerging and niche skills like Neo4j (graph databases) and GDPR (data compliance) are less common but present.
- */
+SELECT skills,
+  ROUND(AVG(salary_year_avg), 0) AS avg_salary
+FROM job_postings_fact
+  INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+  INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+WHERE job_title_short = 'Data Analyst'
+  AND salary_year_avg IS NOT NULL
+GROUP BY skills
+ORDER BY avg_salary DESC
+LIMIT 25
+  /*
+   This SQL query identifies which skills are associated with the highest average salaries for Data Analyst positions. It gives you insights into which skills are most valuable in terms of compensation.
+   
+   [
+   {
+   "skills": "svn",
+   "avg_salary": "400000"
+   },
+   {
+   "skills": "solidity",
+   "avg_salary": "179000"
+   },
+   {
+   "skills": "couchbase",
+   "avg_salary": "160515"
+   },
+   {
+   "skills": "datarobot",
+   "avg_salary": "155486"
+   },
+   {
+   "skills": "golang",
+   "avg_salary": "155000"
+   },
+   {
+   "skills": "mxnet",
+   "avg_salary": "149000"
+   },
+   {
+   "skills": "dplyr",
+   "avg_salary": "147633"
+   },
+   {
+   "skills": "vmware",
+   "avg_salary": "147500"
+   },
+   {
+   "skills": "terraform",
+   "avg_salary": "146734"
+   },
+   {
+   "skills": "twilio",
+   "avg_salary": "138500"
+   },
+   {
+   "skills": "gitlab",
+   "avg_salary": "134126"
+   },
+   {
+   "skills": "kafka",
+   "avg_salary": "129999"
+   },
+   {
+   "skills": "puppet",
+   "avg_salary": "129820"
+   },
+   {
+   "skills": "keras",
+   "avg_salary": "127013"
+   },
+   {
+   "skills": "pytorch",
+   "avg_salary": "125226"
+   },
+   {
+   "skills": "perl",
+   "avg_salary": "124686"
+   },
+   {
+   "skills": "ansible",
+   "avg_salary": "124370"
+   },
+   {
+   "skills": "hugging face",
+   "avg_salary": "123950"
+   },
+   {
+   "skills": "tensorflow",
+   "avg_salary": "120647"
+   },
+   {
+   "skills": "cassandra",
+   "avg_salary": "118407"
+   },
+   {
+   "skills": "notion",
+   "avg_salary": "118092"
+   },
+   {
+   "skills": "atlassian",
+   "avg_salary": "117966"
+   },
+   {
+   "skills": "bitbucket",
+   "avg_salary": "116712"
+   },
+   {
+   "skills": "airflow",
+   "avg_salary": "116387"
+   },
+   {
+   "skills": "scala",
+   "avg_salary": "115480"
+   }
+   ]
+   */
